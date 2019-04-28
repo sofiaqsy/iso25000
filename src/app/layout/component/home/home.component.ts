5300973e9ad780ab07261f394f956d7877c15edf
Jared Cusi <jaredcusi@MacBook-Pro-de-Jared-2.local>
2019-04-28 15:34:07 -0500

    layout component

--- src/app/layout/component/home/home.component.ts
+++ src/app/layout/component/home/home.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-estadistica',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
